5119041035c98aff9a7833582f99085c86fd6feb
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-02-20 18:09:35 +0700

    feat: add initial prototype

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+interface Env {
+  AI: Ai
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+interface requestBody {
+  prompt: string,
+  model: string
+}
+
+interface Ai {
+  run(model: string, payload: Record<string, string>): Promise<any>
+}
+
+const app = new Hono<{ Bindings: Env }>()
+
+app.post('/', async (c) => {
+  const requestBody = await c.req.json() as requestBody
+  const prompt = requestBody.prompt;
+  const model = requestBody.model;
+  const payload = {prompt}
+  const response = await c.env.AI.run(model, payload)
+  return c.json(response)
 })
 
 export default app
